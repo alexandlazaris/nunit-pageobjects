374ad6eac417c426f9a8ff6f56a06c875c2f94f2
alexandlazaris <alex.lazaris617@gmail.com>
2017-07-28 13:12:11 +1000

    Add POs for testing npmjs.com

---
+++ BDD/Resources/PageObjects/npm/Homepage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BDD.Resources.PageObjects.npm
+{
+    public class Homepage
+    {
+        private IWebDriver driver;
+
+        public Homepage(IWebDriver driver)
+        {
+            this.driver = driver;  
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "input#site-search")]
+        private IWebElement _searchBox;
+
+        [FindsBy(How = How.CssSelector, Using = "form#npm-search")]
+        private IWebElement _form;
+
+        public void searchUsingInput(string search)
+        {// COMPARE AGAINST PREVIOUS FRAMEWORK FINDING,
+            _searchBox.Click();
+            _searchBox.SendKeys(search);
+            _form.Submit();
+        }
+
+    }
+}
---
+++ BDD/Resources/PageObjects/npm/SearchResults.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BDD.Resources.PageObjects.npm
+{
+    public class SearchResults
+    {
+        private IWebDriver driver;
+
+        public SearchResults(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "ul.search-results > div[data-reactid]")]
+        private IList<IWebElement> allSearchResults;
+        
+        public void printElements()
+        {
+            String[] allText = new String[allSearchResults.Count];
+            int i = 0;
+            foreach (IWebElement element in allSearchResults)
+            {
+                Console.WriteLine(element.Text);
+                Console.WriteLine(allText[i]);
+                allText[i++] = element.Text;
+                
+            }
+        }
+
+    }
+}
