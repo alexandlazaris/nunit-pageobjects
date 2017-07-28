48b6128b6bc18dc20c35d9895794e7e5946c5efb
alexandlazaris <alex.lazaris617@gmail.com>
2017-07-28 13:12:31 +1000

    Using this file to test new ... tests

--- BDD/UnitTest1.cs
+++ BDD/UnitTest1.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BDD.Resources;
+using BDD.Resources.PageObjects.npm;
+using OpenQA.Selenium.Support.PageObjects;
 
 namespace BDD
 {
     [TestClass]
     public class UnitTest1
     {
+        Homepage npmHome;
+        SearchResults npmSearchResults;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            npmHome = new Homepage(ObjectSetup.Driver);
+            PageFactory.InitElements(ObjectSetup.Driver, npmHome);
+            npmSearchResults = new SearchResults(ObjectSetup.Driver);
+            PageFactory.InitElements(ObjectSetup.Driver, npmSearchResults);
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
-            ObjectSetup.Driver.Navigate().GoToUrl("http://www.google.com");
+            
+            ObjectSetup.Driver.Navigate().GoToUrl("https://www.npmjs.com/");
+            npmHome.searchUsingInput("hello");
+            npmSearchResults.printElements();
         }
     }
 }
