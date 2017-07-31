4dc2d4e481892c965c0ca20d05505527a1f346b0
alexandlazaris <alex.lazaris617@gmail.com>
2017-07-31 11:53:01 +1000

    Rename tests appropriately

---
+++ BDD/Tests_PageObject/Test_Smoke.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BDD.Resources;
+using BDD.Resources.PageObjects.npm;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace BDD
+{
+    [TestClass]
+    public class Test_Smoke
+    {
+        Homepage npmHome;
+        SearchResults npmSearchResults;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            npmHome = new Homepage(ObjectSetup.Driver);
+            npmSearchResults = new SearchResults(ObjectSetup.Driver);
+            PageFactory.InitElements(ObjectSetup.Driver, npmSearchResults);
+        }
+
+        [TestMethod]
+        public void LinkThroughHeaderNavigation()
+        {
+            ObjectSetup.Driver.Navigate().GoToUrl("https://www.npmjs.com/");
+            npmHome.ClickAllHeaderLinks();
+        }
+
+        [TestMethod]
+        public void LinkThroughFooterNavigation()
+        {
+            ObjectSetup.Driver.Navigate().GoToUrl("https://www.npmjs.com/");
+            npmHome.ClickFooterColumn01Links();
+            npmHome.ClickFooterColumn02Links();
+        }
+
+        [TestMethod]
+        public void SelectASearchResult()
+        {
+            ObjectSetup.Driver.Navigate().GoToUrl("https://www.npmjs.com/");
+            npmHome.SearchUsingInput("hello");
+            npmSearchResults.SelectAResultLink(8);
+        }
+    }
+}
