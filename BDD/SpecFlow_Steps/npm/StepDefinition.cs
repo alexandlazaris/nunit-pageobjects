0ccadaaa19f03ae442d5f764dd74570a2cc34227
alexandlazaris <alex.lazaris617@gmail.com>
2017-08-01 10:13:55 +1000

    Organise step definitions into folders based on PO

---
+++ BDD/SpecFlow_Steps/npm/StepDefinition.cs
@@ -0,0 +1,31 @@
+﻿using BDD.Resources;
+using BDD.Resources.PageObjects.npm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace BDD.SpecFlow_Steps.npm
+{
+    [Binding]
+    public sealed class StepDefinition
+    {
+        Homepage npmHome;
+        SearchResults npmSearchResults;
+
+        [When(@"I navigate to npm homepage")]
+        public void WhenINavigateToNpmHomepage()
+        {
+            npmHome = new Homepage(ObjectSetup.Driver);
+            npmSearchResults = new SearchResults(ObjectSetup.Driver);
+            ObjectSetup.Driver.Navigate().GoToUrl("https://www.npmjs.com/");
+        }
+
+        [When(@"I click on all header links")]
+        public void WhenIClickOnAllHeaderLinks()
+        {
+            
+        }
+    }
+}
